683b38cc776e8807704841ebd604f71c67f49117
cactus857 <nekkocat36@gmail.com>
2024-10-08 13:40:48 +0700

    main

---
+++ WebApplication2/Controllers/MenuController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+namespace WebApplication2.Controllers
+{
+    
+    public class MenuController : Controller
+    {
+        
+
+        // GET: Menu
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult getMenu()
+        {
+            return PartialView();
+        }
+        public ActionResult Readers()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+
+        public ActionResult TarotOnline()
+        {
+           
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult InfoReader()
+        {
+            return View();
+        }
+        public ActionResult Test()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication2/Models/menu.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+
+    using System;
+    using System.Collections.Generic;
+    
+    public partial class menu
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string link { get; set; }
+        public string meta { get; set; }
+        public Nullable<bool> hide { get; set; }
+        public Nullable<int> order { get; set; }
+        public Nullable<System.DateTime> datebegin { get; set; }
+    }
+
+}
